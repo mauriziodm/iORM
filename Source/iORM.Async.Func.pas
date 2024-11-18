009753ce725201920c05d9ef29758bca5f836968
Maurizio Del Magno <mauriziodelmagno@gmail.com>
2024-11-18 11:15:53 +0100

    no message

---
+++ Source/iORM.Async.Func.pas
@@ -0,0 +1,138 @@
+unit iORM.Async.Func;
+
+interface
+
+uses
+  System.Classes, System.SysUtils;
+
+type
+
+  TioAsyncFuncExecute<T> = reference to function: T;
+  TioAsyncFuncOnSuccess<T> = reference to procedure(AResultValue: T);
+  TioAsyncFuncOnException = reference to procedure(AException: Exception);
+
+  TioFunc<T> = class
+  public
+    class procedure Invoke(const Async: Boolean; const AOnExecute: TioAsyncFuncExecute<T>; const AOnSuccess: TioAsyncFuncOnSuccess<T>; const AOnException: TioAsyncFuncOnException; const AShowWait: Boolean = False); inline;
+  end;
+
+  TioSyncFunc<T> = class
+  public
+    class procedure Invoke(const AOnExecute: TioAsyncFuncExecute<T>; const AOnSuccess: TioAsyncFuncOnSuccess<T>; const AOnException: TioAsyncFuncOnException; const AShowWait: Boolean = False); inline;
+  end;
+
+  TioAsyncFunc<T> = class(TThread)
+  strict private
+    FException: Exception;
+    FOnExceptionMethod: TioAsyncFuncOnException;
+    FOnExecuteMethod: TioAsyncFuncExecute<T>;
+    FOnSuccessMethod: TioAsyncFuncOnSuccess<T>;
+    FResultValue: T;
+    FShowWait: Boolean;
+  strict protected
+    procedure Execute; override;
+    procedure OnTerminateEventHandler(Sender: TObject);
+  public
+    constructor Create(const AOnExecute: TioAsyncFuncExecute<T>; const AOnSuccess: TioAsyncFuncOnSuccess<T>; const AOnException: TioAsyncFuncOnException; const AShowWait: Boolean = False);
+  end;
+
+implementation
+
+uses
+  iORM;
+
+{ TioAsyncExecutor<T> }
+
+constructor TioAsyncFunc<T>.Create(const AOnExecute: TioAsyncFuncExecute<T>; const AOnSuccess: TioAsyncFuncOnSuccess<T>; const AOnException: TioAsyncFuncOnException; const AShowWait: Boolean = False);
+begin
+  inherited Create(True);
+  FException := nil;
+  FOnExecuteMethod := AOnExecute;
+  FOnSuccessMethod := AOnSuccess;
+  FOnExceptionMethod := AOnException;
+  FShowWait := AShowWait;
+  OnTerminate := OnTerminateEventHandler;
+  FreeOnTerminate := True;
+  if AShowWait then
+    io.ShowWait;
+end;
+
+procedure TioAsyncFunc<T>.Execute;
+begin
+  inherited;
+  try
+    FResultValue := FOnExecuteMethod;
+  except
+    on E: Exception do
+    begin
+      FException := AcquireExceptionObject as Exception;
+      raise;
+    end;
+  end;
+end;
+
+procedure TioAsyncFunc<T>.OnTerminateEventHandler(Sender: TObject);
+begin
+  try
+    // If an exception was raised during the execution of the thread then re-raise the acquire exception into the main thread
+    //  (otherwise I had problems) and then raise a new exception with the same message so that it comes out to the user too.
+    // note: The new exception is raised decoupled with a Timer because I had problems otherwise.
+    if Assigned(FException) then
+                                            begin
+      if FShowWait then
+        io.HideWait;
+      if Assigned(FOnExceptionMethod) then
+        FOnExceptionMethod(FException)
+      else
+        raise FException;
+    end
+    else
+      // If everything went well, it executes the terminate anonymous method
+      if Assigned(FOnSuccessMethod) then
+        FOnSuccessMethod(FResultValue);
+  finally
+    if FShowWait then
+      io.HideWait;
+  end;
+end;
+
+{ TioSyncFunc<T> }
+
+class procedure TioSyncFunc<T>.Invoke(const AOnExecute: TioAsyncFuncExecute<T>; const AOnSuccess: TioAsyncFuncOnSuccess<T>;
+  const AOnException: TioAsyncFuncOnException; const AShowWait: Boolean);
+var
+  LResultValue: T;
+begin
+  if AShowWait then
+    io.ShowWait;
+  try try
+    LResultValue := AOnExecute;
+    if Assigned(AOnSuccess) then
+      AOnSuccess(LResultValue);
+  finally
+    if AShowWait then
+      io.HideWait;
+  end;
+  except
+    on E: Exception do
+    begin
+      if Assigned(AOnException) then
+        AOnException(E)
+      else
+        raise;
+    end;
+  end;
+end;
+
+{ TioFunc<T> }
+
+class procedure TioFunc<T>.Invoke(const Async: Boolean; const AOnExecute: TioAsyncFuncExecute<T>; const AOnSuccess: TioAsyncFuncOnSuccess<T>;
+  const AOnException: TioAsyncFuncOnException; const AShowWait: Boolean);
+begin
+  if Async then
+    TioAsyncFunc<T>.Create(AOnExecute, AOnSuccess, AOnException, AShowWait).Start
+  else
+    TioSyncFunc<T>.Invoke(AOnExecute, AOnSuccess, AOnException, AShowWait);
+end;
+
+end.
---
+++ Source/iORM.Async.Proc.pas
@@ -0,0 +1,135 @@
+unit iORM.Async.Proc;
+
+interface
+
+uses
+  System.SysUtils, System.Classes;
+
+type
+
+  TioAsyncProcExecute = reference to procedure;
+  TioAsyncProcOnSuccess = reference to procedure;
+  TioAsyncProcOnException = reference to procedure(AException: Exception);
+
+  TioProc = class
+  public
+    class procedure Invoke(const Async: Boolean; const AOnExecute: TioAsyncProcExecute; const AOnSuccess: TioAsyncProcOnSuccess; const AOnException: TioAsyncProcOnException; const AShowWait: Boolean = False); inline;
+  end;
+
+  TioSyncProc = class
+  public
+    class procedure Invoke(const AOnExecute: TioAsyncProcExecute; const AOnSuccess: TioAsyncProcOnSuccess; const AOnException: TioAsyncProcOnException; const AShowWait: Boolean = False); inline;
+  end;
+
+  TioAsyncProc = class(TThread)
+  strict private
+    FException: Exception;
+    FOnExceptionMethod: TioAsyncProcOnException;
+    FOnExecuteMethod: TioAsyncProcExecute;
+    FOnSuccessMethod: TioAsyncProcOnSuccess;
+    FShowWait: Boolean;
+  strict protected
+    procedure Execute; override;
+    procedure OnTerminateEventHandler(Sender: TObject);
+  public
+    constructor Create(const AOnExecute: TioAsyncProcExecute; const AOnSuccess: TioAsyncProcOnSuccess; const AOnException: TioAsyncProcOnException; const AShowWait: Boolean = False);
+  end;
+
+implementation
+
+uses
+  iORM;
+
+{ TioAsyncProc }
+
+constructor TioAsyncProc.Create(const AOnExecute: TioAsyncProcExecute; const AOnSuccess: TioAsyncProcOnSuccess; const AOnException: TioAsyncProcOnException;
+  const AShowWait: Boolean);
+begin
+  inherited Create(True);
+  FException := nil;
+  FOnExecuteMethod := AOnExecute;
+  FOnSuccessMethod := AOnSuccess;
+  FOnExceptionMethod := AOnException;
+  FShowWait := AShowWait;
+  OnTerminate := OnTerminateEventHandler;
+  FreeOnTerminate := True;
+  if AShowWait then
+    io.ShowWait;
+end;
+
+procedure TioAsyncProc.Execute;
+begin
+  inherited;
+  try
+    FOnExecuteMethod;
+  except
+    on E: Exception do
+    begin
+      FException := AcquireExceptionObject as Exception;
+      raise;
+    end;
+  end;
+end;
+
+procedure TioAsyncProc.OnTerminateEventHandler(Sender: TObject);
+begin
+  try
+    // If an exception was raised during the execution of the thread then re-raise the acquire exception into the main thread
+    //  (otherwise I had problems) and then raise a new exception with the same message so that it comes out to the user too.
+    // note: The new exception is raised decoupled with a Timer because I had problems otherwise.
+    if Assigned(FException) then
+    begin
+      if FShowWait then
+        io.HideWait;
+      if Assigned(FOnExceptionMethod) then
+        FOnExceptionMethod(FException)
+      else
+        raise FException;
+    end
+    else
+      // If everything went well, it executes the terminate anonymous method
+      if Assigned(FOnSuccessMethod) then
+        FOnSuccessMethod;
+  finally
+    if FShowWait then
+      io.HideWait;
+  end;
+end;
+
+{ TioSyncProc }
+
+class procedure TioSyncProc.Invoke(const AOnExecute: TioAsyncProcExecute; const AOnSuccess: TioAsyncProcOnSuccess; const AOnException: TioAsyncProcOnException; const AShowWait: Boolean = False);
+begin
+  if AShowWait then
+    io.ShowWait;
+  try try
+    AOnExecute;
+    if Assigned(AOnSuccess) then
+      AOnSuccess;
+  finally
+    if AShowWait then
+      io.HideWait;
+  end;
+  except
+    on E: Exception do
+    begin
+      if Assigned(AOnException) then
+        AOnException(E)
+      else
+        raise;
+    end;
+  end;
+end;
+
+{ TioProc }
+
+class procedure TioProc.Invoke(const Async: Boolean; const AOnExecute: TioAsyncProcExecute; const AOnSuccess: TioAsyncProcOnSuccess;
+  const AOnException: TioAsyncProcOnException; const AShowWait: Boolean);
+begin
+  if Async then
+    TioAsyncProc.Create(AOnExecute, AOnSuccess, AOnException, AShowWait).Start
+  else
+    TioSyncProc.Invoke(AOnExecute, AOnSuccess, AOnException, AShowWait);
+end;
+
+end.
