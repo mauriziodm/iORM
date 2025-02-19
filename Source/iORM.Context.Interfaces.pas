793bbbdabd04cdb35008ab80a4e3716968611e4f
Maurizio Del Magno <mauriziodelmagno@gmail.com>
2025-02-19 18:10:46 +0100

    no message

--- Source/iORM.Context.Interfaces.pas
+++ Source/iORM.Context.Interfaces.pas
@@ -39,7 +39,7 @@ uses
   iORM.CommonTypes, iORM.Context.Table.Interfaces,
   iORM.Context.Properties.Interfaces, System.Rtti, iORM.Where.Interfaces,
   iORM.Context.Map.Interfaces, iORM.LiveBindings.BSPersistence,
-  iORM.Attributes, iORM.Auth.Interfaces, iORM.SynchroStrategy.Interfaces;
+  iORM.Attributes, iORM.Auth.Interfaces;
 
 type
 
@@ -66,7 +66,6 @@ type
     function GetCurrentStrategyName: String;
     // Synchronization Strategy methods
     function SynchroStrategy_CanPersistEtmTimeSlot: Boolean;
-    function SynchroStrategy_Client: IioSynchroStrategy_Client;
     procedure SynchroStrategy_GenerateLocalID;
     function SynchroStrategy_GetTimeSlotSynchroState: TioEtmTimeSlotSynchroState;
     function SynchroStrategy_IsToBeSynchronized: Boolean;
@@ -171,142 +170,6 @@ type
     function GetConflictState: TioPersistenceConflictState;
     procedure SetConflictState(const Value: TioPersistenceConflictState);
     property ConflictState: TioPersistenceConflictState read GetConflictState write SetConflictState;
-
-
-
-
-
-
-
-
-
-
-    function GetClassRef: TioClassRef;
-    function GetProperties: IioProperties;
-    function GetTable: IioTable;
-    function GetTrueClass: IioTrueClass;
-    function IDIsNull: Boolean;
-    function IsTrueClass: Boolean;
-    function RttiContext: TRttiContext;
-    function RttiType: TRttiInstanceType;
-    function WhereExist: Boolean;
-    // Conflict strategy methods (to avoid circular reference)
-    procedure CheckDeleteConflict;
-    procedure CheckInsertConflict;
-    procedure CheckUpdateConflict;
-    procedure ResolveDeleteConflict;
-    procedure ResolveInsertConflict;
-    procedure ResolveUpdateConflict;
-    function GetCurrentStrategyName: String;
-    // Synchronization Strategy methods
-    function SynchroStrategy_CanPersistEtmTimeSlot: Boolean;
-    procedure SynchroStrategy_GenerateLocalID;
-    function SynchroStrategy_GetTimeSlotSynchroState: TioEtmTimeSlotSynchroState;
-    // BlindLevel helper methods
-    function BlindLevel_Do_DetectObjExists: boolean;
-    function BlindLevel_Do_AutoUpdateProps: boolean;
-    function BlindLevel_Do_DetectConflicts: boolean;
-    procedure BlindLevel_Set_DetectObjExists;
-    procedure BlindLevel_Set_AutoUpdateProps;
-    procedure BlindLevel_Set_DetectConflicts;
-    procedure BlindLevel_Reset_DetectObjExists;
-    procedure BlindLevel_Reset_AutoUpdateProps;
-    procedure BlindLevel_Reset_DetectConflicts;
-    // Map
-    function Map: IioMap;
-    // DataObject
-    procedure SetDataObject(const AValue: TObject);
-    function GetDataObject: TObject;
-    property DataObject:TObject read GetDataObject write SetDataObject;
-    // ObjID
-    function GetObjID: Integer;
-    procedure SetObjID(const AValue: Integer);
-    property ObjID: Integer read GetObjID write SetObjID;
-    // ObjectStatus
-    procedure SetObjStatus(const AValue: TioObjStatus);
-    function GetObjStatus: TioObjStatus;
-    property ObjStatus:TioObjStatus read GetObjStatus write SetObjStatus;
-    // ObjVersion
-    function GetObjVersion: TioObjVersion;
-    procedure SetObjVersion(const AValue: TioObjVersion);
-    property ObjVersion:TioObjVersion read GetObjVersion write SetObjVersion; // write SetObjVersion;
-    // ObjNextVersion
-    function GetObjNextVersion: Integer; // Con tipo TioObjVersion ci sono problemi
-    property ObjNextVersion: Integer read GetObjNextVersion; // Con tipo TioObjVersion ci sono problemi
-    // ObjCreated
-    function GetObjCreated: TioObjCreated;
-    procedure SetObjCreated(const AValue: TioObjCreated);
-    property ObjCreated: TioObjCreated read GetObjCreated write SetObjCreated;
-    // ObjCreatedUserID
-    function GetObjCreatedUserID: TioObjCreatedUserID;
-    procedure SetObjCreatedUserID(const AValue: TioObjCreatedUserID);
-    property ObjCreatedUserID: TioObjCreatedUserID read GetObjCreatedUserID write SetObjCreatedUserID;
-    // ObjCreatedUserName
-    function GetObjCreatedUserName: TioObjCreatedUserName;
-    procedure SetObjCreatedUserName(const AValue: TioObjCreatedUserName);
-    property ObjCreatedUserName: TioObjCreatedUserName read GetObjCreatedUserName write SetObjCreatedUserName;
-    // ObjUpdatedUserID
-    function GetObjUpdatedUserID: TioObjUpdatedUserID;
-    procedure SetObjUpdatedUserID(const AValue: TioObjUpdatedUserID);
-    property ObjUpdatedUserID: TioObjUpdatedUserID read GetObjUpdatedUserID write SetObjUpdatedUserID;
-    // ObjUpdatedUserName
-    function GetObjUpdatedUserName: TioObjUpdatedUserName;
-    procedure SetObjUpdatedUserName(const AValue: TioObjUpdatedUserName);
-    property ObjUpdatedUserName: TioObjUpdatedUserName read GetObjUpdatedUserName write SetObjUpdatedUserName;
-    // ObjUpdated
-    function GetObjUpdated: TioObjUpdated;
-    procedure SetObjUpdated(const AValue: TioObjUpdated);
-    property ObjUpdated: TioObjUpdated read GetObjUpdated write SetObjUpdated;
-    // Where
-    procedure SetWhere(const AWhere: IioWhere);
-    function GetWhere: IioWhere;
-    property Where:IioWhere read GetWhere write SetWhere;
-    // GroupBy
-    function GetGroupBySql: String;
-    // OrderBy
-    function GetOrderBySql: String;
-    // RelationOID
-    function GetRelationOID: Integer;
-    procedure SetRelationOID(const Value: Integer);
-    property RelationOID: Integer read GetRelationOID write SetRelationOID;
-    // MasterPropertyPath
-    function GetMasterPropertyPath: String;
-    property MasterPropertyPath: String read GetMasterPropertyPath;
-    // MasterBSPersistence
-    function GetMasterBSPersistence: TioBSPersistence;
-    property MasterBSPersistence: TioBSPersistence read GetMasterBSPersistence;
-    // OriginalResolvedTypeNameNonTrueClass
-    procedure SetOriginalNonTrueClassMap(const AMap: IioMap);
-    function GetOriginalNonTrueClassMap: IioMap;
-    property OriginalNonTrueClassMap: IioMap read GetOriginalNonTrueClassMap write SetOriginalNonTrueClassMap;
-    // EntityFromVersion
-    function GetEntityFromVersion: Integer;
-    procedure SetEntityFromVersion(const Value: Integer);
-    property EntityFromVersion: Integer read GetEntityFromVersion write SetEntityFromVersion;
-    // ActionType
-    function GetActionType: TioPersistenceActionType;
-    procedure SetActionType(const Value: TioPersistenceActionType);
-    property ActionType: TioPersistenceActionType read GetActionType write SetActionType;
-    // IntentType
-    function GetIntentType: TioPersistenceIntentType;
-    procedure SetIntentType(const Value: TioPersistenceIntentType);
-    property IntentType: TioPersistenceIntentType read GetIntentType write SetIntentType;
-    // BlindLevel
-    function GetBlindLevel: Byte;
-    procedure SetBlindLevel(const Value: Byte);
-    property BlindLevel: Byte read GetBlindLevel write SetBlindLevel;
-    // ConflictDetected
-    function GetConflictDetected: Boolean;
-    procedure SetConflictDetected(const Value: Boolean);
-    property ConflictDetected: Boolean read GetConflictDetected write SetConflictDetected;
-    // ConflictState
-    function GetConflictState: TioPersistenceConflictState;
-    procedure SetConflictState(const Value: TioPersistenceConflictState);
-    property ConflictState: TioPersistenceConflictState read GetConflictState write SetConflictState;
-    // SessionData
-    function GetSessionData: IioAuthSessionData;
-    procedure SetSessionData(const Value: IioAuthSessionData);
-    property SessionData: IioAuthSessionData read GetSessionData write SetSessionData;
   end;
 
 implementation
